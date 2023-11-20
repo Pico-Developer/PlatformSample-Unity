e6d5f5004da561a34c98bd78db9d2038e24d5a86
dawnhanchen <dawn.han@bytedance.com>
2023-11-20 15:45:40 +0800

    Add CloudStorage Sample and update SDK 2.4.0

---
+++ Assets/Samples/CloudStorage/CloudStorageSample.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pico.Platform.Samples.Game
+{
+    public class CloudStorageSample : MonoBehaviour
+    {
+        public Text debugText;
+        public void StartNewBackup()
+        {
+            string debugMsg= "";
+            debugMsg += "\nStartNewBackup Start";
+
+            CloudStorageService.StartNewBackup().OnComplete(msg =>
+            {
+                if (msg.IsError)
+                {
+                    debugMsg += "\nStartNewBackup IsError, ErrorMessage:" + msg.Error.Message + " ErrorCode:" + msg.Error.Code;
+                }
+                else
+                {
+                    debugMsg = "\nStartNewBackup Successfully";
+                }
+
+                Debug.Log(debugMsg);
+                debugText.text = debugMsg;
+            });
+
+            Debug.Log(debugMsg);
+            debugText.text = debugMsg;
+        }
+    }
+}
